ef51bb289c62485f473603ebb3ee50ef580167fe
Hyunmok-Park <mokssi2@naver.com>
2022-06-21 15:22:22 +0900

    Upload project files

---
+++ src/run_train_container.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+python main.py --network_type rnn --dataset ptb --controller_optim adam --controller_lr 0.00035 --controller_max_step=20 --controller_hid=32 --shared_max_step=20 --shared_hid=32 --shared_embed=32 --shared_optim sgd --shared_lr 20.0 --entropy_coeff 0.0001 --num_blocks=4 --max_epoch=10
+
